ddff89192f79a0fd32ebf51d3bce1d64c7e8586b
Suraj Chamate <124590074+Surajchamate15@users.noreply.github.com>
2023-11-05 18:17:59 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,17 +1,13 @@
 # Design n-Queens matrix having first Queen placed. Use backtracking to place remaining Queens to generate the final 8-queen‟s matrix.
-
 def is_safe(board, row, col):
-    # Check if there's a Queen in the same column
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-    # Check if there's a Queen in the upper-left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check if there's a Queen in the upper-right diagonal
     for i, j in zip(range(row, -1, -1), range(col, len(board))):
         if board[i][j] == 1:
             return False
@@ -37,15 +33,16 @@ def print_board(board):
     for row in board:
         print(" ".join(["Q" if cell == 1 else "." for cell in row]))
 
+
 if __name__ == "__main__":
-    n = 4
-    board = [[0] * n for _ in range(n)]
 
-    # Place the first Queen
-    board[0][1] = 1
+    n = 8
+    initial_board = [[0 for _ in range(n)] for _ in range(n)]
 
-    if solve_n_queens(board, 1):
-        print("4-Queens Solution:")
-        print_board(board)
+    initial_board[0][1] = 1
+
+    if solve_n_queens(initial_board, 1):
+        print("Solution exists. The 8-queens matrix is:")
+        print_board(initial_board)
     else:
-        print("No solution found.")
+        print("No solution exists for the given configuration.")
